c9e454ac2a2e8e5715d9fd3e16352f8b047bd8e4
RaulDuarteBeltran <raul_duarteb@hotmail.com>
2023-12-08 08:04:52 -0600

    Agregar campos a IJuego
    - Juego terminado indica si ya no se jugarán mas rondas y ya hay un ganador.
    - Jugar ronda ejecuta una sola ronda de cada juego. Ahora iniciar juego serviría para inicializar valores u objetos

--- ProyectoOrdinario/ProyectoOrdinario/Interfaces/IJuego.cs
+++ ProyectoOrdinario/ProyectoOrdinario/Interfaces/IJuego.cs
@@ -4,8 +4,10 @@ namespace ProyectoOrdinario.Interfaces
 	public interface IJuego
 	{
 		IDealer Dealer { get; }
+		bool JuegoTerminado { get; }
 		void AgregarJugador(IJugador jugador);
 		void IniciarJuego();
+		void JugarRonda();
 		void MostrarGanador();
 	}
 }
