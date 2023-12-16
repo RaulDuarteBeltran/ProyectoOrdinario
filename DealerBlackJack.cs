bd3912dab6084fd5de811189bb373416d5d984fb
RobertoYB <ryupit26@gmail.com>
2023-12-16 03:47:41 -0600

    Creacion de clase de dealer
    Aquí puse todas las funciones de la clase dealer black jack y dealer poker

---
+++ DealerBlackJack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class DealerBlackJack	//Clase del dealer del juego BlackJack21
+{
+    public BarajearDeck() //Función para barajear el deck
+    {
+
+    }
+
+    public RepartirCartas() //Función para repartir las cartas del deck a los jugadores (en este caso no juega el dealer)
+    {
+
+    }
+
+    public AnalizarMano() //El dealer revisa si tiene menos de 17 puntos para seguir jugando
+    {
+
+    }
+
+    public ComerCarta() //Aqui el dealer revisa su mano y checa si tiene menos de 17 puntos para comer una del mazo
+    {
+
+    }
+
+    public CambiarCartaJugador() //Para intercambiar las cartas que el jugador quiere descartar y enviar a la pila de descartes
+    {
+
+    }
+
+    public TerminarJugada() //El dealer al tener mas de 16 puntos ya deja de jugar y guarda su jugada
+    {
+
+    }
+}
---
+++ DealerPoker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class DealerPoker	//Clase del dealer del juego Poker
+{
+    public BarajearDeck() //Función para barajear el deck
+    {
+
+    }
+
+    public RepartirCartas() //Función para repartir las cartas del deck a los jugadores (en este caso no juega el dealer)
+    {
+
+    }
+
+    public CambiarCartas() //Para intercambiar las cartas que el jugador quiere descartar y enviar a la pila de descartes
+    {
+
+    }
+}
